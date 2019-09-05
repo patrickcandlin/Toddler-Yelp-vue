5fd2ba0a6a3399c5b2f0dc837d29050a0cceda2a
“Patrick <pctennislessons@gmail.com>
2019-09-05 14:03:39 -0600

    functionality of search form now works

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,5 @@ export default {
 </script>
 
 <style>
-#app {
-}
-
 
 </style>
--- src/components/activites/Activites.vue
+++ src/components/activites/Activites.vue
@@ -1,13 +1,43 @@
 <template>
     <section>
-        <router-link to="/activites">
-            <h2>Activities</h2>
-        </router-link>
-        <div class="container" >
-
-            <Card  v-for='business in businessArray' :name='business.name' :image_url='business.image_url' /> 
-                    
-        </div>
+        <b-container  >
+            <router-link to="/activites">
+                <h2>Activities</h2>
+            </router-link>
+        </b-container>
+        <b-container>
+            <b-form
+                @submit="fusionSearch()"
+                v-on:submit.prevent
+                inline
+                class="min-vw-100 p-3"
+                > 
+                <b-form-input 
+                    size="md"
+                    style="width: 24%;"
+                    class="mr-sm-2" 
+                    placeholder="Address, City or Zip" 
+                    v-model="searchForm.location" 
+                    >
+                </b-form-input>
+                <b-form-input 
+                    size="md"
+                    style="width: 24%;"
+                    class="mr-sm-2"
+                    placeholder="Search" 
+                    v-model="searchForm.searchTerm" 
+                    >
+                </b-form-input>
+                <b-button size="md" class="my-2 my-sm-0" type="submit">Search</b-button>
+            </b-form>  
+        </b-container>
+        <b-container>
+            <Card  
+                v-for='business in businessArray' 
+                :name='business.name' 
+                :image_url='business.image_url' 
+            /> 
+        </b-container>
     </section>
 </template>
 <script>
@@ -18,16 +48,40 @@ export default {
     data() {
         return {
             businessArray: [],
+            searchForm: {
+                location: '',
+                searchTerm: '',
+            }
         }
     },
     components: {
         Card
     },
+    methods: {
+        fusionSearch() {
+            const { location, searchTerm } = this.searchForm
+            axios
+                .get('http://localhost:3000/fusion/index', {
+                    params: {
+                        term: searchTerm, location: location
+                        }
+                    }
+                )
+                .then((response) =>{
+                    console.log(response)
+                    this.businessArray = response.data.businesses
+                }) 
+        }
+    },
     mounted() {
         axios
-        .get('http://localhost:3000/fusion/index')
+        .get('http://localhost:3000/fusion/index', {
+                params: {
+                        term: "playground", location: "80210"
+                        }
+                    }
+            )
         .then((response) => {
-                console.log(response.data.businesses)
                 this.businessArray = response.data.businesses
             }
         )
